7b150cb2be6a245a98c9aff5b00079f1800b6ba4
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-18 10:53:19 +0200

    refine the structure and metaelements of the product model

--- models/Product.py
+++ models/Product.py
@@ -4,5 +4,8 @@ from odoo import models, fields
 
 class Product (models.Model):
     _name = 'clinical_management_system.product' #identifier
+    _description = "resources, products, and services offered by the clinic"
+    _order = "date_added desc, name"
     name = fields.Char('Description',required=True)
     fees = fields.Float()
+    date_added = fields.Date('creation date')
