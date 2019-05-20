2990a7fd9d98d666b6579c83963b0fa6fe264118
eman <eman.zayan95@gmail.com>
2019-05-20 14:49:20 +0200

    add patient model with views

--- __manifest__.py
+++ __manifest__.py
@@ -27,6 +27,8 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/patient_view.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
