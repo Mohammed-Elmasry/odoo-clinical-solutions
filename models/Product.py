e1a6377ed02af18098984a54491585b64366c63f
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-21 14:23:04 +0200

    build views for Product model after inheritance

--- models/Product.py
+++ models/Product.py
@@ -4,8 +4,10 @@ from odoo import models, fields
 
 class Product (models.Model):
     _name = 'clinical_management_system.product' #identifier
+    _inherit = "product.template"
     _description = "resources, products, and services offered by the clinic"
     _order = "date_added desc, name"
     name = fields.Char('Description',required=True)
-    fees = fields.Float()
+    fees = fields.Monetary('fees', currency_field='currency_id')
+    currency_id = fields.Many2one('res_currency', string="Currency")
     date_added = fields.Date('creation date')
