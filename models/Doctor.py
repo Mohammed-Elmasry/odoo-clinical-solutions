2a9143cc62e58a834e45a4cd57c2fa1979e3288b
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-21 21:32:11 +0200

    install clinical_management_system module

--- models/Doctor.py
+++ models/Doctor.py
@@ -3,5 +3,5 @@ from odoo import fields, models
 class Doctor_info(models.Model):
     _name = "doctor.info.model"
     _inherit = 'hr.employee'
-    role = fields.Selection(["doctor", "Doctor"], ["officer", "Officer"], ["nurse", "Nurse"], required=True)
+    role = fields.Selection([("doctor", "Doctor"), ("officer", "Officer"), ("nurse", "Nurse")], required=True)
     speciality = fields.Char(size=50)
