2adb31161c860699d8a1922ad96d6c2a1df4372e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-21 21:29:07 +0200

    define doctor_info model

---
+++ models/Doctor.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+class Doctor_info(models.Model):
+    _name = "doctor.info.model"
+    _inherit = 'hr.employee'
+    role = fields.Selection(["doctor", "Doctor"], ["officer", "Officer"], ["nurse", "Nurse"], required=True)
+    speciality = fields.Char(size=50)
