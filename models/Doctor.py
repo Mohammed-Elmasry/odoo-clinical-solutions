096f46566023da94b8b7f1de6cc2831d24f4577b
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-21 22:06:48 +0200

    build action and view for model

--- models/Doctor.py
+++ models/Doctor.py
@@ -2,6 +2,6 @@ from odoo import fields, models
 
 class Doctor_info(models.Model):
     _name = "doctor.info.model"
-    _inherit = 'hr.employee'
+    _inherit = 'product.template'
     role = fields.Selection([("doctor", "Doctor"), ("officer", "Officer"), ("nurse", "Nurse")], required=True)
     speciality = fields.Char(size=50)
