646574f1bdcc636493c257c94bf423cde0eb83bf
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-25 15:10:33 +0200

    clean code

--- models/Doctor.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-class Doctor_info(models.Model):
-    _name = "doctor.info.model"
-    _inherit = 'product.template'
-    role = fields.Selection([("doctor", "Doctor"), ("officer", "Officer"), ("nurse", "Nurse")], required=True)
-    speciality = fields.Char(size=50)
