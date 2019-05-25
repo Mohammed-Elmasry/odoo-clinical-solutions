dbfa76fc3842490bbafe01a90a58fedb30706076
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-25 13:47:55 +0200

    add change

--- models/doctor.py
+++ models/doctor.py
@@ -8,5 +8,5 @@ class DoctorInfo(models.Model):
     emp_id = fields.Many2one('hr.employee')
     role = fields.Selection([("doctor", "Doctor"), ("officer", "Officer"), ("nurse", "Nurse")], required=True)
     speciality = fields.Char(size=50)
-    personal_phone = fields.Char('Phone Number', size=25)
-    personal_address = fields.Text('Address', size=50)
+    # personal_phone = fields.Char('Phone Number', size=25)
+    # personal_address = fields.Text('Address', size=50)
