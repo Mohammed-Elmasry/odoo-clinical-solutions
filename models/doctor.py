85022607fee289ff3626d577ba7c2064ecd1646e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-25 12:07:12 +0200

    changes in the model

--- models/doctor.py
+++ models/doctor.py
@@ -3,8 +3,10 @@ from odoo import fields, models
 
 class DoctorInfo(models.Model):
     _name = "doctor.info.model"
-    _inherit = 'hr.employee'
+    _inherits = {'hr.employee': 'emp_id'}
+
+    emp_id = fields.Many2one('hr.employee')
     role = fields.Selection([("doctor", "Doctor"), ("officer", "Officer"), ("nurse", "Nurse")], required=True)
     speciality = fields.Char(size=50)
-    personal_phone = fields.Text('Phone Number', size=25)
+    personal_phone = fields.Char('Phone Number', size=25)
     personal_address = fields.Text('Address', size=50)
