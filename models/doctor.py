2cdbd8f933a5ea7029e2831c09909490c67c7be8
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-26 10:19:39 +0200

    make override for the gender field in hr.employee class to make it required

--- models/doctor.py
+++ models/doctor.py
@@ -10,5 +10,10 @@ class DoctorInfo(models.Model):
     speciality = fields.Char(size=50)
     license_id = fields.Integer(size=40 , string="License ID")
     name = fields.Char(related='resource_id.name', store=True, oldname='name_related', readonly=False, required=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ], groups="hr.group_hr_user", default="male",required=True )
     # personal_phone = fields.Char('Phone Number', size=25)
     # personal_address = fields.Text('Address', size=50)
