9527251d0e27c9cf328b28ca9bb04cae980f420e
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-28 14:23:42 +0200

    define xPath view for products creation form

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+import  json
 
-# class ClinicalManagementSystem(http.Controller):
-#     @http.route('/clinical_management_system/clinical_management_system/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class ClinicalManagementSystem(http.Controller):
+    @http.route('/clinical_management_system/clinical_management_system/', auth='public')
+    def index(self, **kw):
+        request = http.request
+        vals = kw.keys()
+        # print("Your values are: ",vals)
+        # print("Your Email is: ", kw["Email"])
+        # for i in http.request.env["product.template"]:
+        #     print(i)
+        return  json.dumps({'id': 'yy'})
 
-    # @http.route('/clinical_management_system/clinical_management_system/objects/', auth='public')
-    # def list(self, **kw):
-    #     return http.request.render('clinical_management_system.listing', {
-    #         'root': '/clinical_management_system/clinical_management_system',
-    #         'objects': http.request.env['clinical_management_system.clinical_management_system'].search([]),
-    #     })
+    @http.route('/clinical_management_system/clinical_management_system/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('clinical_management_system.listing', {
+            'root': '/clinical_management_system/clinical_management_system',
+            'objects': http.request.env['res.partner'].search([]),
+        })
 
-    # @http.route('/clinical_management_system/clinical_management_system/objects/<model("clinical_management_system.clinical_management_system"):obj>/', auth='public')
-    # def object(self, obj, **kw):
-    #     return http.request.render('clinical_management_system.object', {
-    #         'object': obj
-    #     })
+    @http.route('/clinical_management_system/clinical_management_system/objects/<model("clinical_management_system.clinical_management_system"):obj>/', auth='public')
+    def object(self, obj, **kw): ##this is a particular object
+        return http.request.render('clinical_management_system.object', {
+            'object': obj
+        })
--- models/Product.py
+++ models/Product.py
@@ -6,7 +6,7 @@ class Product (models.Model):
     _inherit = "product.template"
     _description = "resources, products, and services offered by the clinic"
     _order = "date_added desc, name"
-    name = fields.Char('Description',required=True)
+    name = fields.Char('Name',required=True)
     fees = fields.Monetary('fees', currency_field='currency_id')
     currency_id = fields.Many2one('res_currency', string="Currency")
     date_added = fields.Date('creation date')
