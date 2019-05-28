386f689f1bb176bb36c48edfa93799725f3d483c
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-28 15:58:01 +0200

    add Renaming to the field admission_type

--- models/Visit.py
+++ models/Visit.py
@@ -6,7 +6,7 @@ class Visit(models.Model):
     patient_class=fields.Char(string="Patient class",required ='true')
     set_id = fields.Integer(string="Set ID")
     assigned_patient_location = fields.Char(string="Assigned Patient Location")
-    admission_type=fields.Char()
+    admission_type = fields.Char(string="Admission Type")
     preadmit_number=fields.Integer()
     prior_patient_location=fields.Char()
     # attending_doctor=
