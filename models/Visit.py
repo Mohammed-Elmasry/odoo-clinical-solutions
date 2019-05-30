424aaf07aa0998ab0e72ec72b11a58398b25305f
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-30 11:29:09 +0200

    add naming and comment for transfer bad debt date

--- models/Visit.py
+++ models/Visit.py
@@ -46,7 +46,8 @@ class Visit(models.Model):
     # This field indicates the amount of interest that will be charged
     transfer_bad_debt_code = fields.Integer(string="Bad Debt Code")
     # This field indicates that the account was transferred to bad debts and gives the reason.
-    transfer_bad_debt_date=fields.Date()
+    transfer_bad_debt_date = fields.Date(string="Bad Debt Date")
+    # This field contains the date that the account was transferred to a bad debt status.
     bad_debt_agency_code=fields.Integer()
 
     bad_debt_transfer_amount=fields.Integer()
