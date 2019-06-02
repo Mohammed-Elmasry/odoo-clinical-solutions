8563af2edc381e692d19794f391b9c1d3c889033
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-06-02 12:08:23 +0200

    change the field type for admit source to be selection

--- models/Visit.py
+++ models/Visit.py
@@ -24,7 +24,7 @@ class Visit(models.Model):
     prior_temporary_location = fields.Text(string="Previous Temporary Location")
     preadmit_test_indicator = fields.Char(string="Preadmit Test Indicator")
     re_admission_indicator = fields.Char(string="Re-Admission Indicator")
-    admit_source = fields.Char(string="Admit Source")
+    admit_source = fields.Selection([('value', 'No suggested values defined')], string="Admit Source")
     ambulatory_status = fields.Char(string="Ambulatory Status")
     vip_indicator = fields.Char(string="VIP-Type")
     admitting_doctor = fields.Char(string="Admitting Doctor")
