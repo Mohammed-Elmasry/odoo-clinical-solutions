5d9b4890650d3b00f4567031fe113a9f7faa07f9
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-06-16 10:05:15 +0200

    add attribute domain in admitting_doctor to get doctors only

--- models/Visit.py
+++ models/Visit.py
@@ -56,7 +56,8 @@ class Visit(models.Model):
                                           ('B3', 'Amputee')], string="Ambulatory Status")
     vip_indicator = fields.Char(string="VIP-Type")
     admitting_doctor = fields.Many2one('doctor.info.model', string="Admitting Doctor"
-                                       , help="This field contains the admitting physician information.")
+                                       , help="This field contains the admitting physician information."
+                                       , domain="[('role', '=', 'doctor')]")
     patient_type = fields.Selection([('value', 'No suggested values defined')])
     visit_number = fields.Integer(string="Visit Number",
                                   help="This field contains the unique number assigned to each patient visit.")
